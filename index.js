9d4a4a41f3f052d1a6b0d7091baa038b5fa8c76f
shail10 <shailkardani10@gmail.com>
2025-03-14 02:01:21 -0300

    Added store-file api endpoints - 3

--- index.js
+++ index.js
@@ -8,8 +8,39 @@ const PORT = 6000
 
 app.use(express.json())
 
+STORAGE_PATH = '/shail_PV_dir'
+
 app.get('/', (req, res) => {
   res.send('Container 1 running')
+})
+
+app.post('/store-file', (req, res) => {
+  const { file, data } = req.body
+
+  if (!file) {
+    return res.status(400).json({
+      file: null,
+      error: 'Invalid JSON input.',
+    })
+  }
+
+  try {
+    const filePath = path.join(STORAGE_PATH, file)
+
+    fs.writeFileSync(filePath, data)
+
+    return res.status(200).json({
+      file: file,
+      message: 'Success.',
+    })
+  } catch (err) {
+    console.error('File Write Error:', err.message)
+
+    return res.status(500).json({
+      file: file,
+      error: 'Error while storing the file to the storage.',
+    })
+  }
 })
 
 app.listen(PORT, () => {
